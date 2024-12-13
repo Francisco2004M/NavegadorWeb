17a270aa860556e7441c38c0ee06a9d95bb067cb
Francisco Mendez <fm7074978@gmail.com>
2024-12-13 14:57:12 -0400

    Agregando titulo y icono al proyecto

--- NavegadorWeb/src/main/java/edu/uagrm/fich/navegadorweb/App.java
+++ NavegadorWeb/src/main/java/edu/uagrm/fich/navegadorweb/App.java
@@ -2,19 +2,22 @@ package edu.uagrm.fich.navegadorweb;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
 
 /**
  * JavaFX App
  */
 public class App extends Application {
-    
+
     @Override
     public void start(Stage stage) {
         InterfazGrafica interfazGrafica = new InterfazGrafica();
         Scene scene = new Scene(interfazGrafica, 640, 480);
+        stage.setTitle("Navegador Web UAGRM-FICH");
         stage.setScene(scene);
+        Image image = new Image("/images/icono.png");
+        stage.getIcons().add(image);
         stage.show();
     }
 
