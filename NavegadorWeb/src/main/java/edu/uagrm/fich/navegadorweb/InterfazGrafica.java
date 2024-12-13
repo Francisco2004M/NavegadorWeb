51ccdf22b71bcb8b1120c55e5ba80dbadee8b056
Francisco Mendez <fm7074978@gmail.com>
2024-12-13 17:27:55 -0400

    Registrando navegacion a partir de la interaccion con el webview

--- NavegadorWeb/src/main/java/edu/uagrm/fich/navegadorweb/InterfazGrafica.java
+++ NavegadorWeb/src/main/java/edu/uagrm/fich/navegadorweb/InterfazGrafica.java
@@ -2,6 +2,7 @@ package edu.uagrm.fich.navegadorweb;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.concurrent.Worker;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
@@ -19,7 +20,7 @@ import javafx.scene.web.WebView;
  * @author Francisco
  */
 public class InterfazGrafica extends VBox {
-    
+
     private final TextField txtBuscador = new TextField();
     private final Button btnBuscar = new Button("Buscar");
     private final Button btnAtras = new Button();
@@ -27,10 +28,10 @@ public class InterfazGrafica extends VBox {
     private final Button btnHistorial = new Button();
     private final HBox boxBuscar = new HBox(5, txtBuscador, btnBuscar, btnAtras, btnAdelante, btnHistorial);
     private final Historial historial;
-    
+
     private final WebView webView = new WebView();
     WebEngine webEngine = webView.getEngine();
-    
+
     public InterfazGrafica(Historial historial) {
         super(10);
         this.historial = historial;
@@ -39,25 +40,25 @@ public class InterfazGrafica extends VBox {
         configurarAcciones();
         refrescarBotonesDeNavegacion();
     }
-    
+
     private void configurarPresentacion() {
         setPadding(new Insets(15));
         txtBuscador.setMaxWidth(Double.MAX_VALUE);
         HBox.setHgrow(txtBuscador, Priority.ALWAYS);
         agregarIconos();
     }
-    
+
     private void agregarIconos() {
         FontAwesomeIconView buscarIcon = new FontAwesomeIconView(FontAwesomeIcon.SEARCH);
         FontAwesomeIconView atrasIcon = new FontAwesomeIconView(FontAwesomeIcon.ARROW_LEFT);
         FontAwesomeIconView adelanteIcon = new FontAwesomeIconView(FontAwesomeIcon.ARROW_RIGHT);
         FontAwesomeIconView historialIcon = new FontAwesomeIconView(FontAwesomeIcon.HISTORY);
-        
+
         buscarIcon.setSize("16px");
         atrasIcon.setSize("16px");
         adelanteIcon.setSize("16px");
         historialIcon.setSize("16px");
-        
+
         btnBuscar.setGraphic(buscarIcon);
         btnBuscar.setTooltip(new Tooltip("Buscar página indicada en la barra de busqueda"));
         btnAtras.setGraphic(atrasIcon);
@@ -67,7 +68,7 @@ public class InterfazGrafica extends VBox {
         btnHistorial.setGraphic(historialIcon);
         btnHistorial.setTooltip(new Tooltip("Mostrar Historial de Navegación"));
     }
-    
+
     private void configurarAcciones() {
         btnBuscar.setOnAction((evt) -> buscar(txtBuscador.getText()));
         txtBuscador.setOnKeyPressed(evt -> {
@@ -77,8 +78,15 @@ public class InterfazGrafica extends VBox {
         });
         btnAtras.setOnAction(evt -> clickEnAtras());
         btnAdelante.setOnAction(evt -> clickEnAdelante());
+
+        webEngine.getLoadWorker().stateProperty().addListener((observable, oldValue, newValue) -> {
+            if (Worker.State.SUCCEEDED.equals(newValue)) {
+                txtBuscador.setText(webEngine.getLocation());
+                historial.agregarBusqueda(webEngine.getLocation());
+            }
+        });
     }
-    
+
     private void buscar(String consulta) {
         if (consulta.toLowerCase().contains("http") || consulta.toLowerCase().contains("ftp")) {
             webEngine.load(consulta);
@@ -89,28 +97,28 @@ public class InterfazGrafica extends VBox {
             actualizarHistorial(busqueda);
         }
     }
-    
+
     public void mostrarDialogoHistorial(EventHandler evt) {
         btnHistorial.setOnAction(evt);
     }
-    
+
     public void actualizarHistorial(String pagina) {
         historial.agregarBusqueda(pagina);
         refrescarBotonesDeNavegacion();
     }
-    
+
     private void refrescarBotonesDeNavegacion() {
         btnAdelante.setDisable(!historial.hayAdelante());
         btnAtras.setDisable(!historial.hayAtras());
     }
-    
+
     private void clickEnAdelante() {
         String urlAdelante = historial.adelante();
         webEngine.load(urlAdelante);
         txtBuscador.setText(urlAdelante);
         refrescarBotonesDeNavegacion();
     }
-    
+
     private void clickEnAtras() {
         String urlAtras = historial.atras();
         webEngine.load(urlAtras);
