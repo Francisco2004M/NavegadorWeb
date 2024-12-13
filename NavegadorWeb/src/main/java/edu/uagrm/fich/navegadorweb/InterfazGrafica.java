cee30b6a0b75df31af6f027c9e6b73aec61c349f
Francisco Mendez <fm7074978@gmail.com>
2024-12-13 14:56:25 -0400

    Agregando AwesomeIcons a los botones

--- NavegadorWeb/src/main/java/edu/uagrm/fich/navegadorweb/InterfazGrafica.java
+++ NavegadorWeb/src/main/java/edu/uagrm/fich/navegadorweb/InterfazGrafica.java
@@ -1,8 +1,11 @@
 package edu.uagrm.fich.navegadorweb;
 
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
@@ -12,19 +15,18 @@ import javafx.scene.web.WebView;
  *
  * @author Francisco
  */
-public class InterfazGrafica extends VBox{
-    
+public class InterfazGrafica extends VBox {
+
     private TextField txtBuscador = new TextField();
     private Button btnBuscar = new Button("Buscar");
-    private Button btnAtras = new Button("Atras");
-    private Button btnAdelante = new Button("Adelante");
+    private Button btnAtras = new Button();
+    private Button btnAdelante = new Button();
     private HBox boxBuscar = new HBox(5, txtBuscador, btnBuscar, btnAtras, btnAdelante);
 
-    
     private WebView webView = new WebView();
-        
-    public InterfazGrafica(){
-        super(10);        
+
+    public InterfazGrafica() {
+        super(10);
         getChildren().addAll(boxBuscar, webView);
         configurarPresentacion();
     }
@@ -33,6 +35,23 @@ public class InterfazGrafica extends VBox{
         setPadding(new Insets(15));
         txtBuscador.setMaxWidth(Double.MAX_VALUE);
         HBox.setHgrow(txtBuscador, Priority.ALWAYS);
-        
+        agregarIconos();
+    }
+
+    private void agregarIconos() {
+        FontAwesomeIconView buscarIcon = new FontAwesomeIconView(FontAwesomeIcon.SEARCH);
+        FontAwesomeIconView atrasIcon = new FontAwesomeIconView(FontAwesomeIcon.ARROW_LEFT);
+        FontAwesomeIconView adelanteIcon = new FontAwesomeIconView(FontAwesomeIcon.ARROW_RIGHT);
+
+        buscarIcon.setSize("16px");
+        atrasIcon.setSize("16px");
+        adelanteIcon.setSize("16px");
+
+        btnBuscar.setGraphic(buscarIcon);
+        btnBuscar.setTooltip(new Tooltip("Buscar página indicada en la barra de busqueda"));
+        btnAtras.setGraphic(atrasIcon);
+        btnAtras.setTooltip(new Tooltip("Ir a la página anterior en el Historial"));
+        btnAdelante.setGraphic(adelanteIcon);
+        btnAdelante.setTooltip(new Tooltip("Ir a la siguiente Página en el Historial"));
     }
 }
