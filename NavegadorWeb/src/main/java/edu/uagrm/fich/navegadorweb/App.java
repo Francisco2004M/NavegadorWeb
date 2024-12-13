c56f73884978e58d94924b55562af15a52b40e92
Francisco Mendez <fm7074978@gmail.com>
2024-12-13 16:47:36 -0400

    Agregando funcionalidad de Mostrar Historial

--- NavegadorWeb/src/main/java/edu/uagrm/fich/navegadorweb/App.java
+++ NavegadorWeb/src/main/java/edu/uagrm/fich/navegadorweb/App.java
@@ -2,7 +2,9 @@ package edu.uagrm.fich.navegadorweb;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
@@ -36,8 +38,14 @@ public class App extends Application {
     private void mostrarDialogoHistorial() {
         Stage dialog = new Stage();
         dialog.setTitle("Historial de Navegacion");
-        dialog.getIcons().add(icon);
+        dialog.getIcons().add(icon);        
         dialog.initOwner(stage);
+        
+        ListView<String> listView = new ListView<>(); 
+        listView.getItems().addAll(historial.obtenerHistorialCompleto());
+        
+        Scene scene = new Scene(new StackPane(listView), 640, 480);
+        dialog.setScene(scene);
         dialog.initModality(Modality.APPLICATION_MODAL);
         dialog.showAndWait();
     }
